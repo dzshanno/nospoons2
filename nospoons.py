0b4bcf0e2658eb2791d472fd1eb75dac42400e95
David Shannon <dzshanno@yahoo.com>
2024-07-08 17:24:17 +0200

    put grid details into cells[]

--- nospoons.py
+++ nospoons.py
@@ -1,1 +1,27 @@
 # code for no more spoons episode 2
+import sys
+import math
+
+cells =[]
+
+# init values
+
+width = int(input())  # the number of cells on the X axis
+height = int(input())  # the number of cells on the Y axis
+
+# get the details of the grid
+for r in range(height):
+    line = input()  # width characters, each either a number or a '.'
+    for c in range(width):
+        if line[c] != ".":
+            cells.append([r,c])
+        
+print("Debug messages...", file=sys.stderr, flush=True)
+print(cells, file=sys.stderr, flush=True) 
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+
+# Two coordinates and one integer: a node, one of its neighbors, the number of links connecting them.
+print("0 0 2 0 1")
